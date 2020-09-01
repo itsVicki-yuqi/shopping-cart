c43f63002dd8075ba65807514857fa0e9d6f2827
itsVicki-yuqi <vicki.yuqisong@gmail.com>
2020-09-01 13:58:23 -0500

    day2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import { Container, Carousel} from 'react-bootstrap';
-
-import image1 from './images/image01.jpg';
-import image2 from './images/image02.jpg';
+import image2 from './images/image01.jpg';
+import image1 from './images/image02.jpg';
 import image3 from './images/image03.jpg';
+
+
+import { Container, Carousel } from 'react-bootstrap';
 
 function App() {
   return (
@@ -14,18 +15,18 @@ function App() {
       <Container>
         <Header />
         <Carousel>
-          <Carousel.Item>
-            <img className="d-block w-100" src={image2} alt="First slide"/>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={image1} alt="Second slide"/>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={image3} alt="Third slide"/>
-          </Carousel.Item>
-        </Carousel>
+                  <Carousel.Item>
+                        <img className="d-block w-100" src={ image1 } alt="First slide"/>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                        <img className="d-block w-100" src={ image2 } alt="Second slide"/>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                        <img className="d-block w-100" src={ image3 } alt="Third slide"/>
+                  </Carousel.Item>
+            </Carousel>
         <br />
-      <Footer />
+        <Footer />
       </Container>
       
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,9 @@ function Footer() {
       return (
             <footer id="footer">
                   <br />
-                  <Navbar br="light" variant="light">
-
-                        
+                  <Navbar br="light" variant="light" className="justify-content-center">
+                        All right reserved.
                   </Navbar>
-                  <div class="row no-gutters social-container">
-                        <div class="col"><span class="icon mdi mdi-facebook"></span><span>Facebook</span></div>
-                        <div class="col"><span class="icon mdi mdi-instagram"></span><span>instagram</span></div>
-                        <div class="col"><span class="icon mdi mdi-twitter"></span><span>twitter</span></div>
-                        <div class="col"><span class="icon mdi mdi-youtube-play"></span><span>google</span></div>
-                  </div>
             </footer>
       )
 }
