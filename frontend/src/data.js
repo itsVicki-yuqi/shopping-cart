7a212449a556e8b2f325eb349e377234e4943936
itsVicki-yuqi <vicki.yuqisong@gmail.com>
2020-09-08 16:07:45 -0500

    add backend part

---
+++ frontend/src/data.js
@@ -0,0 +1,60 @@
+export default {
+      products: [
+            {
+                  _id: '1',
+                  name: 'dress',
+                  image: '/images/women1.jpg',
+                  price: 70,
+                  rating: 4.5,
+                  numReviews: 10,
+                  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for.'
+            },
+            {
+                  _id: '2',
+                  name: 'dress',
+                  image: '/images/women2.jpg',
+                  price: 70,
+                  rating: 4.5,
+                  numReviews: 10,
+                  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for.'
+            },
+            {
+                  _id: '3',
+                  name: 'dress',
+                  image: '/images/women3.jpg',
+                  price: 70,
+                  rating: 4.5,
+                  numReviews: 10,
+                  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for.'
+            },
+            {
+                  _id: '4',
+                  name: 'dress',
+                  image: '/images/women4.jpg',
+                  price: 70,
+                  rating: 4.5,
+                  numReviews: 10,
+                  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for.'
+            },
+            {
+                  _id: '5',
+                  name: 'dress',
+                  image: '/images/women5.jpg',
+                  price: 70,
+                  rating: 4.5,
+                  numReviews: 10,
+                  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for.'
+            },
+            {
+                  _id: '6',
+                  name: 'dress',
+                  image: '/images/women6.jpg',
+                  price: 70,
+                  rating: 4.5,
+                  numReviews: 10,
+                  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for.'
+            },
+
+      ]
+
+}
